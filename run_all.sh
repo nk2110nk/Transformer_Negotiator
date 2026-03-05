f674de2b00c4a0553e0525c9f3638f15e9481d4f
nakata <nk2110nk@icloud.com>
2026-03-05 03:32:35 +0000

    実行コマンド

--- run_all.sh
+++ run_all.sh
@@ -2,32 +2,34 @@
 
 set -e  # エラーが出たら停止
 
-# 1. 全部まとめて実行
+# 1. general
 python3 ./train.py -a Boulware Conceder Linear -i Laptop ItexvsCypress IS_BT_Acquisition Grocery thompson Car EnergySmall_A
 
-# 2. Boulware
-python3 ./train.py -a Boulware -i Laptop
-python3 ./train.py -a Boulware -i ItexvsCypress
-python3 ./train.py -a Boulware -i IS_BT_Acquisition
-python3 ./train.py -a Boulware -i Grocery
-python3 ./train.py -a Boulware -i thompson
-python3 ./train.py -a Boulware -i Car
-python3 ./train.py -a Boulware -i EnergySmall_A
+# 2. expert
+issues=(
+  Laptop
+  ItexvsCypress
+  IS_BT_Acquisition
+  Grocery
+  thompson
+  Car
+  EnergySmall_A
+)
 
-# 3. Conceder
-python3 ./train.py -a Conceder -i Laptop
-python3 ./train.py -a Conceder -i ItexvsCypress
-python3 ./train.py -a Conceder -i IS_BT_Acquisition
-python3 ./train.py -a Conceder -i Grocery
-python3 ./train.py -a Conceder -i thompson
-python3 ./train.py -a Conceder -i Car
-python3 ./train.py -a Conceder -i EnergySmall_A
+agent_sets=(
+  "Boulware"
+  "Boulware Conceder"
+  "Boulware Linear"
+  "Conceder Linear"
+  "Conceder"
+  "Linear"
+)
 
-# 4. Linear
-python3 ./train.py -a Linear -i Laptop
-python3 ./train.py -a Linear -i ItexvsCypress
-python3 ./train.py -a Linear -i IS_BT_Acquisition
-python3 ./train.py -a Linear -i Grocery
-python3 ./train.py -a Linear -i thompson
-python3 ./train.py -a Linear -i Car
-python3 ./train.py -a Linear -i EnergySmall_A
+for agents in "${agent_sets[@]}"
+do
+  for issue in "${issues[@]}"
+  do
+    echo "Running: -a $agents -i $issue"
+    python3 ./train.py -a $agents -i $issue
+  done
+done
